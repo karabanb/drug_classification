b0c703a6146efa0bf1b468066522b38551f40a8e
karabanb <bkarabands@gmail.com>
2020-03-23 00:32:39 +0100

    report in progress..

--- scripts/400_PerformanceComaprasion.R
+++ scripts/400_PerformanceComaprasion.R
@@ -12,10 +12,14 @@ load('data/300_glm_data.RData')
 load('data/301_trees_data.RData')
 
 load('tmp/300_train_index.RData')
+load('tmp/300_glm_performace.RData')
+load('tmp/301_rpart_performace.RData')
+load('tmp/302_rf_performace.Rdata')
 
 load('results/300_m3_glm.RData')
 load('results/301_m4_rpart.RData')
 load('results/302_m6_rf.RData')
+
 
 
 # Making Expaliners ----------------------------------------------------------------------------------------------------
@@ -48,6 +52,9 @@ eval_rpart <- model_evaluation(exp_rpart)
 eval_rf <- model_evaluation(exp_rf)
 
 plot(eval_glm, eval_rpart, eval_rf, type = 'roc')
+
+results_df <- bind_rows(perf_glm, perf_rpart, perf_rf)
+save(results_df, file = 'results/400_performance_metrics.RData')
 
 rm(list = ls())
 
